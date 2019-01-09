0a63768c656c1e8206b14f5ed948b5dc3d91c429
Alberto <alberto.goujon@gmail.com>
2019-01-09 15:39:28 +0100

    Primer commit

---
+++ Calculadora/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        //aquí declaro las variables de  instancia
+        double operando1 = 0;   //operando1 guardará e primer número
+        String operacion = "";   //En el string operación guardo la operación que ha sido ejecutada
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            if(label1.Text == "0")
+            {
+                label1.Text = boton.Text;
+            }
+            else
+            {
+                label1.Text += boton.Text; 
+            }
+            
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            operacion = boton.Text;
+            operando1 = Convert.ToDouble(label1.Text);
+            label1.Text = "0";
+
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            double operando2 = Convert.ToDouble(label1.Text);
+            double resultado = 0;
+            if(operacion == "+")
+            {
+                resultado = operando2 + operando1;
+            }
+            else if(operacion == "-")
+            {
+                resultado = operando1 - operando2;
+            }
+
+            label1.Text = Convert.ToString(resultado);
+        }
+    }
+}
